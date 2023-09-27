afaf596517a809bdf570da6bddabe3405d5c1412
Sandra Calzada <sndr157@gmail.com>
2023-09-27 12:36:37 +0500

    simple basic setup

--- flaskapp.py
+++ flaskapp.py
@@ -1,1 +1,15 @@
 
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, COW!'
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo MoooooOOoOo!'
---
+++ lib.py
@@ -0,0 +1,3 @@
+def power(x, y):
+    print("UPDATED")
+    return x**y
