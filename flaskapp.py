d55a7ca67b5fffb129a0f0a33cc1834bbb9695ea
sndr157 <sandra.157@hotmail.com>
2023-09-28 13:00:40 +0500

    routes to /

--- flaskapp.py
+++ flaskapp.py
@@ -6,10 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 # Assign a function to be called when the path `/` is requested
-@app.route('/api/')
+@app.route('/')
 def index():
     return 'Hello, COW!'
 
-@app.route('/api/cow')
+@app.route('/cow')
 def cow():
     return 'MOoooOo MoooooOOoOo!'
